29c54dbae033d76b9cab1d2503e3cf83fa2605bb
Marcus Willock <crazcalm@gmail.com>
2022-04-03 20:15:36 -0400

    broadcast example

---
+++ broadcast/src/main.rs
@@ -0,0 +1,39 @@
+use tokio::sync::broadcast;
+
+async fn process_1(msg: String) {
+    println!("Process 1 --> msg: {}", msg);
+}
+
+async fn process_2(msg: String) {
+    println!("Process 2 --> msg: {}", msg);
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx1) = broadcast::channel(16);
+
+    // Creates a new receiver
+    let mut rx2 = tx.subscribe();
+
+    let handle_1 = tokio::spawn(async move {
+        while let Ok(msg) = rx1.recv().await {
+            process_1(msg).await;
+        }
+    });
+
+    let handle_2 = tokio::spawn(async move {
+        while let Ok(msg) = rx2.recv().await {
+            process_2(msg).await;
+        }
+    });
+
+    tx.send("data set 1".to_string()).unwrap();
+    tx.send("data set 2".to_string()).unwrap();
+
+    // Dropping tx so that the recievers will close.
+    drop(tx);
+
+    // Making sure the handlers have finished
+    handle_1.await.unwrap();
+    handle_2.await.unwrap();
+}
