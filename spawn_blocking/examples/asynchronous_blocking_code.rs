fb9df4ab30b309b3953db09536788d7f32311657
Marcus Willock <crazcalm@gmail.com>
2022-04-08 09:36:46 -0400

    Improving the spwan blocking code examples

--- spawn_blocking/examples/asynchronous_blocking_code.rs
+++ spawn_blocking/examples/asynchronous_blocking_code.rs
@@ -12,26 +12,18 @@ fn sync_code(input: i32) -> i32 {
 async fn main() {
     let input = 0;
 
-    // example of sync code being called directly
-    let _ = sync_code(0);
-
-    // Example of sync code being called with spawn blocking.
     let blocked_code = task::spawn_blocking(move || {
         let result = sync_code(input);
 
         result
-    })
-    .await
-    .unwrap();
+    });
 
     let blocked_code2 = task::spawn_blocking(move || {
         let result = sync_code(input);
 
         result
-    })
-    .await
-    .unwrap();
+    });
 
-    println!("result: {:?}", blocked_code);
-    println!("result: {:?}", blocked_code2);
+    println!("result: {:?}", blocked_code.await.unwrap());
+    println!("result: {:?}", blocked_code2.await.unwrap());
 }
